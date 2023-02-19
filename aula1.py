fe22e0ef17af8f79537fc3187cf7eb76e3d4c7a3
rbkretzer <rafael.bkretzer@gmail.com>
2023-02-19 20:09:23 -0300

    inicio sobre tipos

--- aula1.py
+++ aula1.py
@@ -1,2 +1,14 @@
+"""
+
+DocString, não é comentário mas é utilizado para documentação
+
+"""
+'''
+
+DocString, não é comentário mas é utilizado para documentação
+
+'''
+
+# comentário de linha
 print('Hello, world!')
-print('teste de mudança')
+print(123)
---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('Rafael Kretzer')
+print(1, 'Rafael "Kretzer"')
+
+# Aspas duplas
+print("Rafael Kretzer")
+print(2, "Rafael 'Kretzer'")
+
+# Escape
+print('Rafael \'Kretzer\'')
+
+# r
+print(r'Rafael \'Kretzer\'')
